f15f37ff5bb6f26b8f4086c3659298cd64375fc9
Gnakale junior <leognakale@gmail.com>
2026-04-21 15:18:48 +0200

    feat: ajout fonction nettoyage des valeurs nulles

--- src/transformations.py
+++ src/transformations.py
@@ -6,3 +6,13 @@ Chaque fonction doit :
 - prendre un DataFrame en entrée
 - retourner un DataFrame transformé
 """
+
+def clean_nulls(df):
+    """
+    Supprime toutes les lignes contenant des valeurs nulles.
+
+    Explication :
+    Spark stocke les données sous forme de DataFrame distribué.
+    dropna() supprime les lignes où au moins une colonne est NULL.
+    """
+    return df.dropna()
