dcf5c0d9e53373e10f9b37d8946d58d9dd7e2e42
Emmanuel-Kouassi-Esse <emmanuelkouassiesse@gmail.com>
2026-04-21 15:59:06 +0200

    feat: ajout agrégation des ventes

--- src/transformations.py
+++ src/transformations.py
@@ -6,3 +6,19 @@ Chaque fonction doit :
 - prendre un DataFrame en entrée
 - retourner un DataFrame transformé
 """
+
+from pyspark.sql.functions import avg
+
+def average_sales(df):
+    """
+    Calcule la moyenne des ventes par région.
+
+    groupBy :
+    regroupe les données par colonne (comme SQL GROUP BY)
+
+    agg :
+    applique une fonction d’agrégation
+    """
+    # Chaque région aura une seule ligne dans le résultat,
+    # avec la moyenne des ventes calculée sur toutes ses lignes.
+    return df.groupBy("region").agg(avg("sales").alias("avg_sales"))
