8b660dd7b792aead4d5cba3fd27417cb0885711b
doua01 <douajeanbaptiste2001@gmail.com>
2026-04-21 20:18:04 +0000

    feat: ajout de la fonction pour renommer les colonnes

--- src/transformations.py
+++ src/transformations.py
@@ -6,3 +6,8 @@ Chaque fonction doit :
 - prendre un DataFrame en entrée
 - retourner un DataFrame transformé
 """
+def rename_columns(df):
+    """
+    Renomme les colonnes pour améliorer la lisibilité.
+    """
+    return df.withColumnRenamed("old_name", "new_name")
